9abe9b8ccc10aadbf807157d5def952543d1ae03
marellanorero <marellanonorero@gmail.com>
2022-05-16 23:12:03 +0000

    commit

--- exercises/05-Create-DOM-Element-First/index.js
+++ exercises/05-Create-DOM-Element-First/index.js
@@ -1,1 +1,5 @@
 // Your code here
+let elem = document.createElement("p");
+elem.innerHTML = "Hello world";
+elem.style.background = "yellow"; 
+document.body.appendChild(elem);
--- exercises/06-Create-DOM-Element-Second/index.js
+++ exercises/06-Create-DOM-Element-Second/index.js
@@ -1,3 +1,9 @@
-//you code here
-document.body.innerHTML = "<h1>Hello World</h1>";
+let html = "<img src="https://via.placeholder.com/350x150">"
+let elem = document.getElementById("myDiv");
+div.innerHTML = html;
 
+
+let el = document.getElementById("myDiv");
+el.innerHTML = '<img ">';
+
+document.body.innerHTML = "<h1>Hello World</h1>";
